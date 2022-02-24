1773bfcb00001f1129287f93815042665f8eebe1
Donte Ladatto <donte279@gmail.com>
2022-02-24 15:45:31 -0600

    commit tree df275485f4db60e9b15075c64ff0ea5d427fe5e7

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+let thanks = [];
+function writeCards(cards, event) {
+  for (let i = 0; i < cards.length; i++) {
+    thanks.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+  }
+  return thanks;
+}
+
+function countDown() {
+let countUp = 10;
+while (countUp >= 0) {
+console.log(countUp--);
+}
+}
